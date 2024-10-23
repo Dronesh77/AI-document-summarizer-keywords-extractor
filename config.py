f897cbc5cb0e8acbdfb9265a60028d91a6d1f0da
Dronu <dmm12042002@gmail.com>
2024-10-23 16:43:23 +0530

    updated config.py database name

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # MongoDB Configuration
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://dronesh_magare:Mark77@mon@35.160.120.126:27017/mydatabase")  # Change to your MongoDB URI
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://dronesh_magare:Mark77@mon@35.160.120.126:27017/PDF_Summary")  # Change to your MongoDB URI
 # MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")  # Mongo DB uri for local host
 DATABASE_NAME = os.getenv("DATABASE_NAME", "PDF_Summary")  # MongoDB database name
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "processed_pdfs")  # MongoDB collection name
